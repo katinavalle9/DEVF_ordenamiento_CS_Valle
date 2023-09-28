d14757dfc60e114bd79bfbcc07280e44f753a4de
Katia Valle <katinavalle9@gmail.com>
2023-09-28 10:27:47 -0600

    Modificaciones en la clase

--- js/ordenamiento.js
+++ js/ordenamiento.js
@@ -23,46 +23,46 @@ console.log(
 
 //Ejercicio3
 class Alumno {
-  constructor(nombre, calificacion) {
-    this.nombre = nombre;
-    this.calificacion = calificacion;
+    constructor(nombre, calificaciones) {
+      this.nombre = nombre;
+      this.calificaciones = calificaciones;
+    }
+  
+    obtenerPromedio() {
+      const promedio = this.calificaciones.reduce((acc, calificacion) => acc + calificacion, 0) / this.calificaciones.length;
+      return promedio;
+    }
   }
-
-  obtenerPromedio(calificacion) {
-    const promedio =
-      this.calificaciones.reduce((acc, calificacion) => acc + calificacion, 0) /
-      this.calificaciones.length;
-    return promedio;
+  
+  class Salon {
+    constructor() {
+      this.alumnos = [];
+    }
+  
+    agregarAlumno(alumno) {
+      this.alumnos.push(alumno);
+    }
+  
+    static ordenarPorPromedio(alumnos) {
+      alumnos.sort((a, b) => b.obtenerPromedio() - a.obtenerPromedio());
+    }
   }
-}
-
-class Salon {
-  constructor() {
-    this.alumnos = [];
-  }
-
-  agregarAlumno(alumno) {
-    this.alumnos.push(alumno);
-  }
-
-  ordenarPorPromedio() {
-    this.alumnos.sort((a, b) => b.obtenerPromedio() - a.obtenerPromedio());
-  }
-}
-
-const alumno1 = new Alumno("Carlos", [90, 85, 78, 92, 88]);
-const alumno2 = new Alumno("Jose", [95, 88, 76, 89, 100]);
-const alumno3 = new Alumno("Melina", [88, 90, 82, 87, 78]);
-
-const salon = new Salon();
-salon.agregarAlumno(alumno1);
-salon.agregarAlumno(alumno2);
-salon.agregarAlumno(alumno3);
-
-salon.ordenarPorPromedio();
-
-console.log("Alumnos ordenados por promedio:");
-console.log(salon.alumnos);
+  
+  // Ejemplo de uso:
+  const alumno1 = new Alumno("Jose", [90, 85, 78, 92, 88]);
+  const alumno2 = new Alumno("Carlos", [95, 88, 76, 89, 92]);
+  const alumno3 = new Alumno("Perla", [88, 90, 82, 87, 78]);
+  
+  const salon = new Salon();
+  salon.agregarAlumno(alumno1);
+  salon.agregarAlumno(alumno2);
+  salon.agregarAlumno(alumno3);
+  
+  Salon.ordenarPorPromedio(salon.alumnos);
+  
+  console.log("Alumnos ordenados por promedio:");
+  console.log(salon.alumnos);
+  
 
 //Ejercicio5
 function coordenadas() {
